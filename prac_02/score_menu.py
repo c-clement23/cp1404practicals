3dae8eb4fa13431c6fb623eab7758dfecfdc1edb
c-clement23 <carolclement@my.jcu.edu.au>
2025-05-31 22:41:53 +0630

    Validate Score function completed

---
+++ prac_02/score_menu.py
@@ -0,0 +1,30 @@
+def main():
+    menu = ("(G)et a valid score\n"
+            "(P)rint result\n"
+            "(S)how stars\n"
+            "(Q)uit")
+    print(menu)
+    choice = input(">>> ").upper()
+    while choice != "Q":
+        if choice == "G":
+            score = int(input("Enter Score: "))
+            score = validate_score(score)
+
+
+def validate_score(score):
+    while score < 0 or score > 100:
+        print("Invalid Score (must be in 0 - 100 range)")
+        score = int(input("Enter Score: "))
+    return score
+
+def determine_score(score):
+    if score < 0 or score > 100:
+        grade = "Invalid score"
+    elif score >= 90:
+        grade = "Excellent"
+    elif score >= 50:
+        grade = "Passable"
+    else:
+        grade = "Bad"
+    return grade
+main()
