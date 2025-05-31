372665946e083f4157645e5e0c95d297b5bb8176
c-clement23 <carolclement@my.jcu.edu.au>
2025-05-31 22:27:27 +0630

    Added random score generation to determine generated grade

--- prac_02/score.py
+++ prac_02/score.py
@@ -4,6 +4,7 @@
 CP1404/CP5632 - Practical
 Broken program to determine score status
 """
+import random
 
 # TODO: Fix this!
 
@@ -26,6 +27,9 @@ def main():
     score = get_score()
     grade = determine_score(score)
     print(grade)
+    random_score = random.randint(0,100)
+    random_grade = determine_score(random_score)
+    print(random_grade)
 
 main()
 
