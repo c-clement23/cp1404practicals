bb08327030ee3b573abb32a7ad8544b883393abd
c-clement23 <carolclement@my.jcu.edu.au>
2025-05-31 22:15:27 +0630

    Committing Temperatures and Score file before refactoring

--- prac_02/temperatures.py
+++ prac_02/temperatures.py
@@ -8,17 +8,28 @@ F - Convert Fahrenheit to Celsius
 Q - Quit"""
 print(MENU)
 choice = input(">>> ").upper()
+
+
+def convert_to_fahrenheit(celsius):
+    fahrenheit = celsius * 9.0 / 5 + 32
+    return fahrenheit
+
+
+def convert_to_celsius(fahrenheit):
+    celsius = 5 / 9 * (fahrenheit - 32)
+    return celsius
+
 while choice != "Q":
     if choice == "C":
         celsius = float(input("Celsius: "))
-        fahrenheit = celsius * 9.0 / 5 + 32
+        fahrenheit = convert_to_fahrenheit(celsius)
         print(f"Result: {fahrenheit:.2f} F")
     elif choice == "F":
         # TODO: Write this section to convert F to C and display the result
         # Hint: celsius = 5 / 9 * (fahrenheit - 32)
         # Remove the "pass" statement when you are done. It's a placeholder.
         fahrenheit = float(input("Fahrenheit: "))
-        celsius = 5 / 9 * (fahrenheit - 32)
+        celsius = convert_to_celsius()
         print(f"Result: {celsius:.2f} C")
     else:
         print("Invalid option")
