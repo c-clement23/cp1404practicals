ac27c0d311b2b3db867b88ebf134bab2a56bddd0
c-clement23 <carolclement@my.jcu.edu.au>
2025-05-31 22:23:35 +0630

    Refactored Score file

--- prac_02/score.py
+++ prac_02/score.py
@@ -7,12 +7,28 @@ Broken program to determine score status
 
 # TODO: Fix this!
 
-score = float(input("Enter score: "))
-if score < 0 or score > 100:
-    print("Invalid score")
-elif score >= 90:
-    print("Excellent")
-elif score >= 50:
-    print("Passable")
-else:
-    print("Bad")
+def get_score():
+    score = float(input("Enter score: "))
+    return score
+
+def determine_score(score):
+    if score < 0 or score > 100:
+        grade = "Invalid score"
+    elif score >= 90:
+        grade = "Excellent"
+    elif score >= 50:
+        grade = "Passable"
+    else:
+        grade = "Bad"
+    return grade
+
+def main():
+    score = get_score()
+    grade = determine_score(score)
+    print(grade)
+
+main()
+
+
+
+
