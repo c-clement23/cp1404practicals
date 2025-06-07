6b5ed9c08f9971717fda2cb9d967e63c4c39cd65
c-clement23 <carolclement@my.jcu.edu.au>
2025-06-07 19:48:51 +0630

    Random numbers output answered in commented lines as well as coding an output that prints a number from 1 to 100

---
+++ prac_03/randoms.py
@@ -0,0 +1,15 @@
+# Line 1: 12
+# Smallest Number: 5
+# Largest Number: 20
+
+# Line 2: 7
+# Smallest Number: 3
+# Largest Number: 9
+# This code cannot produce 4
+
+# Line 3: 3.504472998284388
+# Smallest Number: 2.5
+# Largest Number:5.5
+
+import random
+print(random.randint(1, 100))
