10546f4f29af23234231d9588981d9be0e5d87c8
c-clement23 <carolclement@my.jcu.edu.au>
2025-06-07 20:31:59 +0630

    Answered the questions and changed the code to prevent ZeroDivisionError

---
+++ prac_03/exceptions_demo.py
@@ -0,0 +1,23 @@
+"""
+CP1404/CP5632 - Practical
+Answer the following questions:
+1. When will a ValueError occur?
+2. When will a ZeroDivisionError occur?
+3. Could you change the code to avoid the possibility of a ZeroDivisionError?
+"""
+# 1) It will occur when a String Value is entered into a variable that is meant to contain an Integer Value.
+# 2) It will occur when a value either an Integer or Float is divided by 0.
+# 3) Yes it is possible.
+try:
+    numerator = int(input("Enter the numerator: "))
+    denominator = int(input("Enter the denominator: "))
+    while denominator == 0:
+        print("Cannot divide by 0")
+        denominator = int(input("Enter the denominator: "))
+    fraction = numerator / denominator
+    print(fraction)
+except ValueError:
+    print("Numerator and denominator must be valid numbers!")
+# except ZeroDivisionError:
+#     print("Cannot divide by zero!")
+print("Finished.")
