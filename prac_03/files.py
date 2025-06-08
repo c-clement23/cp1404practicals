b8be507cb2a76bf787487d707cd25147d9d31591
c-clement23 <carolclement@my.jcu.edu.au>
2025-06-08 14:39:35 +0630

    Read file and add the first 2 numbers then output the total

--- prac_03/files.py
+++ prac_03/files.py
@@ -1,11 +1,19 @@
-# 1) Open and Close file after writing
-name = input("Enter your name: ")
-out_file = open("name.txt", 'w')
-print(name, file=out_file)
-out_file.close()
+# # 1) Open and Close file after writing
+# name = input("Enter your name: ")
+# out_file = open("name.txt", 'w')
+# print(name, file=out_file)
+# out_file.close()
+#
+# # 2) Open and read the previous saved text file and output it
+# in_file = open("name.txt", 'r')
+# name = in_file.read().strip()
+# in_file.close()
+# print(f"Hi {name}!")
 
-# 2) Open and read the previous saved text file and output it
-in_file = open("name.txt", 'r')
-name = in_file.read().strip()
-in_file.close()
-print(f"Hi {name}!")
+# 3) Read file and add the first 2 numbers
+with open("numbers", 'r') as in_file:
+    first_number = int(in_file.readline())
+    second_number = int(in_file.readline())
+
+total = first_number + second_number
+print(total)
