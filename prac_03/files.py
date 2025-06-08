f29df28987422f38e5b3ffde8d12fc9d124377b6
c-clement23 <carolclement@my.jcu.edu.au>
2025-06-08 14:18:48 +0630

    Open and read the previous saved file and output it

--- prac_03/files.py
+++ prac_03/files.py
@@ -1,4 +1,11 @@
+# 1) Open and Close file after writing
 name = input("Enter your name: ")
 out_file = open("name.txt", 'w')
 print(name, file=out_file)
 out_file.close()
+
+# 2) Open and read the previous saved text file and output it
+in_file = open("name.txt", 'r')
+name = in_file.read().strip()
+in_file.close()
+print(f"Hi {name}!")
