c1f2fae52b691462b822ce19f17dc9d088fd3492
c-clement23 <carolclement@my.jcu.edu.au>
2025-06-14 12:31:48 +0630

    Edited the first and last element of numbers and printed according to instructions

--- prac_04/lists_warmup.py
+++ prac_04/lists_warmup.py
@@ -1,4 +1,4 @@
-numbers = [3, 1, 4, 1, 5, 9, 2]
+numbers = ["ten", 1, 4, 1, 5, 9, 1]
 # numbers[0] # 3
 # numbers[-1] # 2
 # numbers[3] # 1
@@ -8,3 +8,5 @@ numbers = [3, 1, 4, 1, 5, 9, 2]
 # 7 in numbers # False
 # "3" in numbers # False
 # numbers + [6, 5, 3] # 3, 1, 4, 1, 5, 9, 2, 6, 5, 3
+print(numbers[2:])
+print(9 in numbers)
