e6ff516ec2a873be52378da2d243239252b92d0b
c-clement23 <carolclement@my.jcu.edu.au>
2025-06-14 12:58:05 +0630

    Refactored the variable months into an appropriate name and refactored the report printing part to a function

--- prac_04/total_income.py
+++ prac_04/total_income.py
@@ -7,18 +7,23 @@ Starter code for cumulative total income program
 def main():
     """Display income report for incomes over a given number of months."""
     incomes = []
-    months = int(input("How many months? "))
+    number_of_months = int(input("How many months? "))
 
-    for month in range(1, months + 1):
+    for month in range(1, number_of_months + 1):
         income = float(input(f"Enter income for month {month}: "))
         incomes.append(income)
 
+    print_report(incomes, number_of_months)
+
+
+def print_report(incomes, number_of_months):
+    """Print Report with Totals after each month"""
     print("\nIncome Report\n-------------")
     total = 0
-    for month in range(1, months + 1):
+    for month in range(1, number_of_months + 1):
         income = incomes[month - 1]
         total += income
-        print(f"Month {months:2} - Income: ${income:10.2f}          Total: ${total:10.2f}")
+        print(f"Month {number_of_months:2} - Income: ${income:10.2f}          Total: ${total:10.2f}")
 
 
 main()
