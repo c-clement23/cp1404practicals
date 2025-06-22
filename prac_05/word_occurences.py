b8864cc3c78c7b8df8ee8cdf679c04bcbce871af
c-clement23 <carolclement@my.jcu.edu.au>
2025-06-22 16:37:15 +0630

    Counted words for each word in text and tested with print statement

--- prac_05/word_occurences.py
+++ prac_05/word_occurences.py
@@ -6,3 +6,15 @@ Estimated Time: 20 mins
 text = input("Text: ")
 
 words = text.split()
+
+
+word_counts = {}
+for word in words:
+    word = word.lower()
+    if word in word_counts:
+        word_counts[word] += 1
+    else:
+        word_counts[word] = 1
+
+sorted(word_counts)
+print(word_counts)
