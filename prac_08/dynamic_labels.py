1f414827a952f7e1121b3456d800e74148ee728f
c-clement23 <carolclement@my.jcu.edu.au>
2025-07-20 14:36:17 +0630

    Built the GUI for the kivy file

--- prac_08/dynamic_labels.py
+++ prac_08/dynamic_labels.py
@@ -5,6 +5,14 @@ from kivy.uix.label import Label
 class DynamicLabelsApp(App):
     """App that dynamically creates Labels for a list of names"""
     def __init__(self, **kwargs):
+        """Construct main app."""
         super().__init__(**kwargs)
 
         self.names = ["Alice", "Bob", "Charlie", "Dana"]
+
+    def build(self):
+        """Build the Kivy GUI."""
+        self.title = "Dynamic Labels"
+        self.root = Builder.load_file('dynamic_labels.kv')
+        self.create_labels()
+        return self.root
