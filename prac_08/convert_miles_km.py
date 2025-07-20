d5c1201cd64b6d350449409f30bde8cf600d938c
c-clement23 <carolclement@my.jcu.edu.au>
2025-07-20 14:02:57 +0630

    Implemented the StringProperty similar to mvc_demo.py file

--- prac_08/convert_miles_km.py
+++ prac_08/convert_miles_km.py
@@ -1,8 +1,13 @@
 from kivy.lang import Builder
 from kivy.app import App
+from kivy.properties import StringProperty
+
+MILES_TO_KILOMETERS = 1.60934
 
 
 class MilesConverterApp(App):
+    result_text = StringProperty("0.0")
+
     def build(self):
         self.title = "convert miles km"
         self.root = Builder.load_file('convert_miles_km.kv')
