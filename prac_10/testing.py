cc587da47e7664948eeafd5473979d8d334bd972
c-clement23 <carolclement@my.jcu.edu.au>
2025-07-31 09:26:43 +0630

    Fix is_long_word function so the tests pass

--- prac_10/testing.py
+++ prac_10/testing.py
@@ -12,17 +12,17 @@ def repeat_string(s, n):
     return ' '.join([s] * n)
 
 
-# def is_long_word(word, length=5):
-#     """
-#     Determine if the word is as long or longer than the length passed in
-#     >>> is_long_word("not")
-#     False
-#     >>> is_long_word("supercalifrag")
-#     True
-#     >>> is_long_word("Python", 6)
-#     True
-#     """
-#     return len(word) > length
+def is_long_word(word, length=5):
+    """
+    Determine if the word is as long or longer than the length passed in
+    >>> is_long_word("not")
+    False
+    >>> is_long_word("supercalifrag")
+    True
+    >>> is_long_word("Python", 6)
+    True
+    """
+    return len(word) >= length
 
 
 def run_tests():
