5fedab0be38edff9002e7a6cbfe49bca9b55f449
c-clement23 <carolclement@my.jcu.edu.au>
2025-07-31 19:24:32 +0630

    Implemented a search query function with loops

--- prac_10/wiki.py
+++ prac_10/wiki.py
@@ -7,6 +7,7 @@ def main():
         page = wikipedia.page(query)
 
         print(f"Title: {page.title}")
+        print(f"Summary: \n{wikipedia.summary(query,sentences=3)}")
         query = input("Enter page title: ")
 
 if __name__ == "__main__":
