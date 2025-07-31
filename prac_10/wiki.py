356dc85955aa24b46c7e9635d050562bb1216962
c-clement23 <carolclement@my.jcu.edu.au>
2025-07-31 19:50:20 +0630

    Implemented an exception handling for disambiguation error

--- prac_10/wiki.py
+++ prac_10/wiki.py
@@ -11,6 +11,10 @@ def main():
             print(f"{wikipedia.summary(query, sentences=3)}")
             print(page.url)
 
+        except wikipedia.exceptions.DisambiguationError as e:
+            print("We need a more specific title. Try one of the following, or a new search:")
+            print(e.options)
+
         except wikipedia.exceptions.PageError:
             print(f'Page id "{query}" does not match any pages. Try another id!')
 
